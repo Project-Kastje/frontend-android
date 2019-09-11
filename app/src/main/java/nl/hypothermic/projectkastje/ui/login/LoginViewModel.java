b707844e6dc3f380c729666ffb9feb6e20931d2a
hypothermic <admin@hypothermic.nl>
2019-09-11 14:11:34 +0200

    Add NL vertalingen
    Signed-off-by: hypothermic <admin@hypothermic.nl>

--- app/src/main/java/nl/hypothermic/projectkastje/ui/login/LoginViewModel.java
+++ app/src/main/java/nl/hypothermic/projectkastje/ui/login/LoginViewModel.java
@@ -1,7 +1,5 @@
 package nl.hypothermic.projectkastje.ui.login;
 
-import android.util.Patterns;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -42,16 +40,17 @@ public class LoginViewModel extends ViewModel {
     }
 
     public void loginDataChanged(String username, String password) {
-        if (!isUserNameValid(username)) {
+        // We hebben geen limieten aan usernames/passwords dus check niet.
+        /*if (!isUserNameValid(username)) {
             loginFormState.setValue(new LoginFormState(R.string.invalid_username, null));
         } else if (!isPasswordValid(password)) {
             loginFormState.setValue(new LoginFormState(null, R.string.invalid_password));
-        } else {
+        } else {*/
             loginFormState.setValue(new LoginFormState(true));
-        }
+        //}
     }
 
-    // A placeholder username validation check
+/*  // A placeholder username validation check
     private boolean isUserNameValid(String username) {
         if (username == null) {
             return false;
@@ -67,4 +66,5 @@ public class LoginViewModel extends ViewModel {
     private boolean isPasswordValid(String password) {
         return password != null && password.trim().length() > 5;
     }
+*/
 }
